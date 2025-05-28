ac0b49932ffcc3be1e16f67b202c87c18ae268f1
Amey Agrawal <ameyagrawal@Ameys-MacBook-Pro.local>
2025-05-27 20:35:19 -0700

    minor

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ from setuptools_scm import get_version
 ROOT_DIR = os.path.dirname(__file__)
 
 
-
 def get_requirements() -> List[str]:
     """Get Python package dependencies from requirements.txt."""
     with open("requirements.txt") as f:
@@ -18,14 +17,14 @@ def get_requirements() -> List[str]:
 setup(
     author="Amey Agrawal",
     author_email="agrawalamey12@gmail.com",
-    python_requires='>=3.10',
+    python_requires=">=3.10",
     description="A CLI dashboard to monitor GPU utilization and other metrics on remote hosts via SSH.",
-    keywords='GPU, Monitor, Dashboard, CLI, SSH, NVIDIA, NVIDIA-SMI, Rich',
-    name='gpu-cluster-monitor',
-    packages=find_packages(include=['gpu_cluster_monitor', 'gpu_cluster_monitor.*']),
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/AgrawalAmey/gpu-cluster-monitor',
+    keywords="GPU, Monitor, Dashboard, CLI, SSH, NVIDIA, NVIDIA-SMI, Rich",
+    name="gpu-cluster-monitor",
+    packages=find_packages(include=["gpu_cluster_monitor", "gpu_cluster_monitor.*"]),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/AgrawalAmey/gpu-cluster-monitor",
     version=get_version(),
     install_requires=get_requirements(),
     entry_points={
